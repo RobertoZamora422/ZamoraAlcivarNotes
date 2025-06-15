0f0d5369d7f6bbac15effb12775967afa172e0b8
RobertoZmora <robertozamora421@gmail.com>
2025-06-15 18:33:01 -0500

    Personalizacion de la pagina About

--- ZamoraAlcivarNotes/Models/About.cs
+++ ZamoraAlcivarNotes/Models/About.cs
@@ -8,8 +8,9 @@ namespace ZamoraAlcivarNotes.Models;
 
 internal class About
 {
-    public string Title => AppInfo.Name;
+    public string Title => "Zamora Alcívar Notes";
     public string Version => AppInfo.VersionString;
-    public string MoreInfoUrl => "https://aka.ms/maui";
-    public string Message => "This app is written in XAML and C# with .NET MAUI.";
+    public string MoreInfoUrl => "https://github.com/RobertoZamora422/ZamoraAlcivarNotes.git";
+    public string Message => "Apasionado por la tecnología y la innovación.";
+    public string Author => "Roberto Zamora Alcívar";
 }
