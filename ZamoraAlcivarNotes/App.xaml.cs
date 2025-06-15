41d3c544da473f9569170d1d0e9988a8f0097318
RobertoZmora <robertozamora421@gmail.com>
2025-06-15 03:15:44 -0500

    Agregar archivos de proyecto.

---
+++ ZamoraAlcivarNotes/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ZamoraAlcivarNotes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
